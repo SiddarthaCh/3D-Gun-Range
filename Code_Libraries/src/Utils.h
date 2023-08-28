a0d7d9f7612e288b105a904f56fc4ab1db8fe0fc
Siddartha Chennareddy <chsiddu2002@gmail.com>
2023-08-28 23:39:38 +0530

    Adding code

---
+++ Code_Libraries/src/Utils.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include<bits/stdc++.h>
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+using namespace std;
+
+void drawText(string text, GLfloat x, GLfloat y, GLfloat z);
+
+void readScorelist(map<string, int>& scoreList);
+
+void writeScorelist(map<string, int>& scoreList, string playerName, int playerScore);
